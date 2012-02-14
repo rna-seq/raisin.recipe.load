94295c2fafb1a1a0db6600cc6a27a80387fb415d
Maik R��der <maikroeder@gmail.com>
2012-02-14 23:23:40 +0000

    move from recipe

--- raisin/recipe/load/__init__.py
+++ raisin/recipe/load/__init__.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 """Recipe apache"""
-from recipe import Recipe
+
+import database
+
+
+class Recipe(object):
+
+    def __init__(self, buildout, name, options):
+        self.buildout = buildout
+        self.name = name
+        self.options = options
+
+    def install(self):
+        staging = self.buildout['transform']['staging']
+        database.main(staging)
+
+    def update(self):
+        return self.install()
